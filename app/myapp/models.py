6967081656f6422f0330139c65208546faac101f
MozyOk <mozy.okubo@gmail.com>
2020-02-17 02:13:47 +0900

    WIP add myapp

---
+++ app/myapp/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class App(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=100)
